5914836a1581ec3c798c1b9c030439f78fe0983b
Paul Garcia <paul.ngarcia@yahoo.com>
2023-01-26 16:51:48 -0600

    EOD Thursday

--- JS/break_and_continue.js
+++ JS/break_and_continue.js
@@ -1,11 +1,32 @@
 
-for (let question = prompt("Pick an odd number between 1 and 50")) {
-    if (question % 2 === 0) {
+
+for (let i = 1; i <50;  i ++) {
+    let question = prompt("Please enter a number between 1 and 50")
+
+    if (question % 2 === 0 && question < 50 ) {
         alert("That was an even number, please pick an ODD number");
 
     }
+    else if (question > 50){
+        alert("that was too high, please keep it under 50");
+
+    }
+    else if (question < 0){
+        alert("That number is too low, please keep it between 1 and 50.");
+
+    }
+
     else {
         alert("Thank you");
         console.log(question);
+
     }
+
+    if (question % 2 !== 0 && question < 50) {
+        console.log(question);
+        question ++;
+
+    }
+    break;
 }
+
