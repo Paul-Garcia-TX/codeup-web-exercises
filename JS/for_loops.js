d7166d85d289cc0a3ff77315ae1c16f36f1e7c2d
Paul Garcia <paul.ngarcia@yahoo.com>
2023-01-26 10:26:50 -0600

    starting work on Thursday

---
+++ JS/for_loops.js
@@ -0,0 +1,48 @@
+
+let question = prompt("PLease enter a number:");
+console.log("The user entered " + question);
+let input = parseInt(question);
+
+function showMultiplicationTable(input){
+    for (let i = 1; i < 11; i++){
+        console.log(input + " x " + i + " = " + (input * i));
+    }
+
+}
+showMultiplicationTable(input);
+//console.log(showMultiplicationTable());
+//console.log("The multiplication table for " + input + " is " + showMultiplicationTable(input));
+//alert("The multiplication table for " + input + " is " + showMultiplicationTable(input));
+
+//I need a random number generator
+//between 20 and 200
+//only generate 10 responses
+/**
+let numberToStop = 200;
+let luckyNumber = Math.floor(Math.random() * 20);
+
+for ( let rando = luckyNumber; luckyNumber < 200; luckyNumber++){
+    if (luckyNumber % 2 === 0 ) {
+        //alert("The number is even");
+        console.log(luckyNumber + " is even");
+    } else {
+       // alert("The number is odd");
+        console.log(luckyNumber + " is odd.");
+    }
+
+}
+ */
+let x=1;
+function cat (x){
+        for (let x = 1; x < 10; x++){
+            console.log(x);
+        return "" + x;
+}
+}
+cat(x);
+
+/**function concat (input, inputOne) {
+
+        return "" + input  + inputOne;
+
+}*/
