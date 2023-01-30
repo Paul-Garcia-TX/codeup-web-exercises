292e793c8d668ad48edec3bfed8bd9e1a34290cc
Paul Garcia <paul.ngarcia@yahoo.com>
2023-01-30 12:35:32 -0600

    finished icecream

--- JS/ice cream shop.js
+++ JS/ice cream shop.js
@@ -1,4 +1,3 @@
-alert("Hi welcome to basket case ice cream!");
 
 // This is how you get a random number between 50 and 100
 var allCones = Math.floor(Math.random() * 50) + 50;
@@ -6,28 +5,35 @@ var allCones = Math.floor(Math.random() * 50) + 50;
 Math.floor(Math.random() * 5) + 1;
 
 
-console.log(allCones);
+
+alert("Hi welcome to basket case ice cream!");
 
 
-console.log(allCones);
 
 
+console.log("Number of cones available: " + allCones);
 
-do {
-    let transaction =  prompt("How many are you buying?");
+while (allCones >= 0) {
+    let transaction = prompt("How many are you buying?");
 
-    let remainder = allCones - transaction;
-    if (transaction > remainder){
-        alert("Ok that will be " + transaction + " ice creams. Thank you!");
+    function selling() {
+        if (transaction <= allCones) {
+            allCones -= transaction;
+            alert("Ok that will be " + transaction + " ice creams. Thank you!");
+            console.log(transaction + " purchased");
+            console.log(allCones + " remaining.");
+        } else if (transaction > allCones) {
+            alert("Sorry, I don't have that many ice cream cones to sell. I only have " + allCones + " to sell.");
+        }
+
+        return allCones;
     }
-    else if (transaction > remainder){
-        alert("Sorry, I don't have that many ice cream cones to sell");
-    }
-    console.log(remainder);
 
+    allCones = selling();
+
+    if (allCones === 0) {
+        alert("Sorry I'm all out of cones to sell. Come back again someday!");
+        break;
+    }
 }
 
-while (allCones < 50){
-    console.log("The amount remaining is " + remainder);
-
-}
