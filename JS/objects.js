cb98ac03afd7fc7028101de599a229285f697d5f
Paul Garcia <paul.ngarcia@yahoo.com>
2023-01-31 14:45:21 -0600

    finished objects exercise

--- JS/objects.js
+++ JS/objects.js
@@ -36,7 +36,10 @@
      * more than $200. If a shopper spends more than $200, they get a 12%
      * discount. Write a JS program, using conditionals, that logs to the
      * browser, how much Ryan, Cameron and George need to pay. We know that
-     * Cameron bought $180, Ryan $250 and George $320. Your program will have to
+     * Cameron bought $180
+     * Ryan $250
+     * and George $320
+     * . Your program will have to
      * display a line with the name of the person, the amount before the
      * discount, the discount, if any, and the amount after the discount.
      *
@@ -45,12 +48,43 @@
      * and console.log the relevant messages for each person
      */
 
-    // var shoppers = [
-    //     {name: 'Cameron', amount: 180},
-    //     {name: 'Ryan', amount: 250},
-    //     {name: 'George', amount: 320}
-    // ];
+     var shoppers = [
+        {name: 'Cameron', amount: 180},
+        {name: 'Ryan', amount: 250},
+        {name: 'George', amount: 320}
+     ];
+   /** function discount(shoppers){
+        if (shoppers.amount > 200){
+            alert("Hi " + shoppers.name + ". You've spent " + shoppers.amount + " dollars. You get a discount of 12%");
+            let doMath = shoppers.amount * 0.12;
+            let newTotal = shoppers.amount - doMath;
+            console.log(shoppers.name +"'s new total is " + newTotal);
+            alert("Your new total is " + newTotal);
+        }
+        else {
+            alert("Sorry, " + shoppers.name + " You have only spent " + shoppers.amount + ". You don't get a discount. Your total is still " + shoppers.amount);
+            console.log(shoppers.name + " total is " + shoppers.amount);
+        }
 
+    } */
+
+    shoppers.forEach( function(shoppers){
+        if (shoppers.amount > 200){
+            alert("Hi " + shoppers.name + ". You've spent " + shoppers.amount + " dollars. You get a discount of 12%");
+            let doMath = shoppers.amount * 0.12;
+            let newTotal = shoppers.amount - doMath;
+            console.log(shoppers.name +"'s new total is " + newTotal);
+            alert("Your new total is " + newTotal);
+        }
+        else {
+            alert("Sorry, " + shoppers.name + " You have only spent " + shoppers.amount + ". You don't get a discount. Your total is still " + shoppers.amount);
+            console.log(shoppers.name + " total is " + shoppers.amount);
+        }
+
+    });
+
+
+    console.log("-------------------");
     /** TODO:
      * Create an array of objects that represent books and store it in a
      * variable named `books`. Each object should have a title and an author
@@ -63,6 +97,21 @@
      * > console.log(books[0].author.firstName) // "Douglas"
      * > console.log(books[0].author.lastName) // "Adams"
      */
+    let books = [
+        {title: 'Harry Potter and the Prisoner of Azkeban', authorFirst: 'JK', authorLast: 'Rowling'},
+        {title: 'A song of Ice and Fire', authorFirst: 'George', authorLast: 'RR. Martin',},
+        {title: 'Ready Player One', authorFirst: 'Ernest', authorLast: 'Cline'},
+        {title: 'Gone Girl', authorFirst: 'Gillian', authorLast: 'Flynn'},
+        {title: 'Where the Crawdads Sing', authorFirst: 'Delia', authorLast: 'Owens'},
+    ]
+
+    books.forEach(function(books,index){
+        console.log("Book #" + (index + 1));
+        console.log(books.title);
+        console.log(books.authorFirst);
+        console.log(books.authorLast);
+        console.log("--------------");
+    });
 
     /**
      * TODO:
