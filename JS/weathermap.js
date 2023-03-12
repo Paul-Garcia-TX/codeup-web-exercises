2e5618f55883564d199f78de6641d08e0d49e45d
Paul Garcia <paul.ngarcia@yahoo.com>
2023-03-11 19:58:10 -0600

    cards in place need configure, map placed and searchbar fixed

--- JS/weathermap.js
+++ JS/weathermap.js
@@ -18,3 +18,82 @@ $.ajax({
     console.log(data);
 });
 
+// mapbox
+mapboxgl.accessToken = 'pk.eyJ1IjoicGF1bG5nYXJjaWEiLCJhIjoiY2xmMm9yanIwMGtpNzNyazdkZDY5NWhpdCJ9.S5vaMxkEfllNbXc2OcT1bA';
+var map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v11',
+    zoom: 10
+
+});
+function geocode(search, token) {
+    var baseUrl = 'https://api.mapbox.com';
+    var endPoint = '/geocoding/v5/mapbox.places/';
+    return fetch(baseUrl + endPoint + encodeURIComponent(search) + '.json' + "?" + 'access_token=' + token)
+        .then(function(res) {
+            return res.json();
+            // to get all the data from the request, comment out the following three lines...
+        }).then(function(data) {
+            return data.features[0].center;
+        });
+}
+geocode("Austin, TX", mapboxgl.accessToken).then(function(result) {
+    console.log(result);
+    map.setCenter(result);
+
+});
+
+
+
+
+
+
+
+
+
+let userinput = "";
+
+$(document).ready(function() {
+    $("#search").click(function() {
+        userinput = $("#searchbar").val();
+        geocode(userinput, mapboxgl.accessToken).then(function(result) {
+
+            map.setCenter(result);
+        });
+
+    });
+});
+
+
+placeMarkersAndPopups(locations, mapboxgl.accessToken, map);
+
+function updateCityName() {
+    var center = map.getCenter();
+    geocode(center.lng + ',' + center.lat, mapboxgl.accessToken).then(function(result) {
+        var city = result.features.find(function(feature) {
+            return feature.place_type.includes('place');
+        });
+        if (city) {
+            var cityName = city.text;
+            document.getElementById('current-city').innerHTML = 'Current City: ' + cityName;
+        }
+    });
+}
+
+$(document).ready(function() {
+    $("#search").click(function() {
+        var userinput = $("#searchbar").val();
+        geocode(userinput, mapboxgl.accessToken).then(function(result) {
+            map.setCenter(result);
+            updateCityName();
+        });
+    });
+});
+
+function updateCurrentCityName(cityName) {
+    var currentCity = document.getElementById("current-city");
+    currentCity.textContent = "Current City: " + cityName;
+}
+
+updateCurrentCityName();
+updateCityName();
