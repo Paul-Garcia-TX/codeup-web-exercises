5c636473a5924689610539ea9551badaf8a1b058
Paul Garcia <paul.ngarcia@yahoo.com>
2023-03-13 11:23:32 -0500

    finished map and adjusted weather map

--- JS/weathermap.js
+++ JS/weathermap.js
@@ -2,17 +2,17 @@
 const mapboxToken = "pk.eyJ1IjoicGF1bG5nYXJjaWEiLCJhIjoiY2xmMm9yanIwMGtpNzNyazdkZDY5NWhpdCJ9.S5vaMxkEfllNbXc2OcT1bA";
 const openWeather = "190a1dcab33040d6dbf6cbd4eded6702";
 
-$.ajax({
-    url: "http://api.openweathermap.org/data/2.5/weather",
-    type: "GET",
-    data: {
-        APPID: openWeather,
-        q:     "Austin, TX, US",
-        units: "imperial",
-    } // <-- missing closing curly brace for `data` object
-}).done(function(data){
-    console.log(data);
-});
+// $.ajax({
+//     url: "http://api.openweathermap.org/data/2.5/weather",
+//     type: "GET",
+//     data: {
+//         APPID: openWeather,
+//         q:     "Austin, TX, US",
+//         units: "imperial",
+//     } // <-- missing closing curly brace for `data` object
+// }).done(function(data){
+//     console.log(data);
+// });
 
 // mapbox
 mapboxgl.accessToken = 'pk.eyJ1IjoicGF1bG5nYXJjaWEiLCJhIjoiY2xmMm9yanIwMGtpNzNyazdkZDY5NWhpdCJ9.S5vaMxkEfllNbXc2OcT1bA';
@@ -33,14 +33,11 @@ function geocode(search, token) {
             return data.features[0].center;
         });
 }
-geocode("Austin, TX", mapboxgl.accessToken).then(function(result) {
+geocode("2090 Airport Rd, New Braunfels, TX", mapboxgl.accessToken).then(function(result) {
     console.log(result);
     map.setCenter(result);
-
+    map.setZoom(15);
 });
-
-
-
 
 
 
@@ -53,57 +50,51 @@ $(document).ready(function() {
     $("#search").click(function() {
         userinput = $("#searchbar").val();
         geocode(userinput, mapboxgl.accessToken).then(function(result) {
-            map.setCenter(result);
-            var mapElement = document.getElementById("map");
-            mapElement.scrollIntoView({behavior: "smooth"});
+
+            map.flyTo({center:result,
+                zoom: 10,
+                speed: 1
+            });
+            var marker = new mapboxgl.Marker()
+                .setLngLat(result)
+                .addTo(map);
+            document.getElementById("map").scrollIntoView();
+
         });
+
     });
 });
-const canvas = map.getCanvasContainer();
 
-const geojson = {
-    'type': 'FeatureCollection',
-    'features': [
-        {
-            'type': 'Feature',
-            'geometry': {
-                'type': 'Point',
-                'coordinates': result
-            }
+const center =map.getCenter().toArray();
+
+$.ajax({
+    url: "https://api.openweathermap.org/data/2.5/weather",
+    type: "GET",
+    data: {
+        APPID: openWeather,
+        lat: center[1],
+        lon: center[0],
+        units: "imperial",
+    }
+}).done(function(data) {
+    console.log(data);
+});
+map.on('moveend', function() {
+    var center = map.getCenter();
+    fetchWeatherData(center.lat, center.lng);
+});
+
+function fetchWeatherData(lat, lon) {
+    $.ajax({
+        url: "https://api.openweathermap.org/data/2.5/weather",
+        type: "GET",
+        data: {
+            APPID: openWeather,
+            lat: lat,
+            lon: lon,
+            units: "imperial",
         }
-    ]
-};
-
-mapboxgl.accessToken = 'pk.eyJ1IjoicGF1bG5nYXJjaWEiLCJhIjoiY2xmMm9yanIwMGtpNzNyazdkZDY5NWhpdCJ9.S5vaMxkEfllNbXc2OcT1bA';
-const mapboxClient = mapboxSdk({ accessToken: mapboxgl.accessToken });
-mapboxClient.geocoding
-    .forwardGeocode({
-        query: 'Austin, TX',
-        autocomplete: false,
-        limit: 1
-    })
-    .send()
-    .then((response) => {
-        if (
-            !response ||
-            !response.body ||
-            !response.body.features ||
-            !response.body.features.length
-        ) {
-            console.error('Invalid response:');
-            console.error(response);
-            return;
-        }
-        const feature = response.body.features[0];
-
-        const map = new mapboxgl.Map({
-            container: 'map',
-// Choose from Mapbox's core styles, or make your own style with Mapbox Studio
-            style: 'mapbox://styles/mapbox/streets-v12',
-            center: feature.center,
-            zoom: 10
-        });
-
-// Create a marker and add it to the map.
-        new mapboxgl.Marker().setLngLat(feature.center).addTo(map);
+    }).done(function(data) {
+        console.log(data);
     });
+}
